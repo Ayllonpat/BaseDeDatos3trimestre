b580311a9720613764e0037910ea8731e6722cc3
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2024-05-02 11:00:31 +0200

    herencias

---
+++ ejemploHerencia/src/main/java/com/salesianostriana/dam/ejemploherencia/EjemploHerenciaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemploherencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploHerenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploHerenciaApplication.class, args);
+	}
+
+}
